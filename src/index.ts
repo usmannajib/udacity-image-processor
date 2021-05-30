a63263c2aed2ac9f784e485049b262ab30246cd6
Usman Najeeb <usman.najib@gmail.com>
2021-05-29 19:59:55 -0700

    Basic routing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Initial commit!")
+import express from "express";
+import router from "./routes";
+
+const app = express();
+const port = 3000;
+
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Server listenting at http://localhost:${port}`);
+});
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import imageRouter from "./api/image";
+
+const router = express.Router();
+
+router.use("/", imageRouter)
+
+export default router;
